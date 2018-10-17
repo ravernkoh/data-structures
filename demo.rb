7aeea08c81607be9a1aca347c16af03cfb7e7daa
Ravern Koh <ravernkoh@gmail.com>
2018-10-18 00:21:19 +0800

    Create pointerDemo

--- demo.rb
+++ demo.rb
@@ -1,4 +1,9 @@
 def main
+  filter = nil
+  if ARGV.count > 0
+    filter = ARGV[0]
+  end
+
   begin
     Dir.mkdir("bin")
   rescue
@@ -6,15 +11,19 @@ def main
 
   dirs = Dir.glob("*")
   dirs = dirs.select { |file| File.directory? file }
-  dirs.each { |dir| demo dir }
+  dirs.each { |dir| demo dir, filter }
 end
 
-def demo(dir)
+def demo(dir, filter)
   files = Dir.glob("#{dir}/*")
-  files.each { |file| run dir, file }
+  files.each { |file| run file, filter }
 end
 
-def run(dir, file)
+def run(file, filter)
+  if filter && !(file.include? filter)
+    return
+  end
+
   case true
   when file.end_with?(".cpp")
     puts "-> Compiling #{file}..."
