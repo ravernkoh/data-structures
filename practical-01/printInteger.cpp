b4e7466c6e80049e13276c91f36694c44e03fbb2
Ravern Koh <ravernkoh@gmail.com>
2018-10-18 00:06:39 +0800

    Create printInteger

--- practical-01/printInteger.cpp
+++ practical-01/printInteger.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <limits>
+#include <math.h>
 
 using namespace std;
 
+#define DIGITS 5
+
 int main() {
-  cout << "Hello, world!" << endl;
-  return 0;
+  int maxN = pow(10, DIGITS) - 1;
+  int minN = pow(10, DIGITS - 1);
+
+  while (true) {
+    cout << "Enter a " << DIGITS << "-digit integer: ";
+    int n;
+    cin >> n;
+
+    if (cin.fail() || n < minN || n > maxN) {
+      cout << "You have entered an invalid number! Try again." << endl;
+
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      continue;
+    }
+
+    for (int i = 0; i < DIGITS; i++) {
+      cout << n / (int)pow(10, DIGITS - i - 1) % 10;
+      if (i < DIGITS - 1) {
+        cout << "   ";
+      }
+    }
+    cout << endl;
+
+    return 0;
+  }
 }
