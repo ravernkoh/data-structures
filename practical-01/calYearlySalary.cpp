787a36cff0149e50f461144d8547f756657abf87
Ravern Koh <ravernkoh@gmail.com>
2018-10-22 11:11:02 +0800

    Split practical-01/calYearlySalary.cpp into multiple files

--- practical-01/calYearlySalary.cpp
+++ practical-01/calYearlySalary.cpp
@@ -1,47 +1,8 @@
 #include <iostream>
-#include <string>
+
+#include "Employee.h"
 
 using namespace std;
-
-class Employee {
-private:
-  string firstName;
-  string lastName;
-  int salary;
-
-public:
-  Employee(string firstName, string lastName, int salary);
-  void setFirstName(string firstName);
-  string getFirstName();
-  void setLastName(string lastName);
-  string getLastName();
-  void setSalary(int salary);
-  int getSalary();
-};
-
-Employee::Employee(string firstName, string lastName, int salary) {
-  this->firstName = firstName;
-  this->lastName = lastName;
-  this->salary = salary;
-}
-
-void Employee::setFirstName(string firstName) { this->firstName = firstName; }
-
-string Employee::getFirstName() { return this->firstName; }
-
-void Employee::setLastName(string lastName) { this->lastName = lastName; }
-
-string Employee::getLastName() { return this->lastName; }
-
-void Employee::setSalary(int salary) {
-  if (salary < 0) {
-    this->salary = 0;
-    return;
-  }
-  this->salary = salary;
-}
-
-int Employee::getSalary() { return this->salary; }
 
 int main() {
   Employee john("John", "Doe", 5000);
