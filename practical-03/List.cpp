3329476bfecc8803c7269dcc033796b1ac91dcc0
Ravern Koh <ravernkoh@gmail.com>
2018-10-25 14:09:30 +0800

    Fix List::add and add List::print

--- practical-03/List.cpp
+++ practical-03/List.cpp
@@ -1,16 +1,35 @@
 #include "List.h"
+#include <iostream>
+
+using namespace std;
 
 List::List() {
   this->firstNode = new Node{Item(), nullptr};
   this->size = 0;
 }
 
-bool List::add(Item item) {
+bool List::add(Item item) { return this->add(this->size, item); }
+
+bool List::add(int index, Item item) {
+  if (index > this->size) {
+    return false;
+  }
+
+  Node *cur = this->firstNode;
+  while (index-- > 0) {
+    cur = cur->next;
+  }
+
+  cur->next = new Node{item, cur->next};
+  this->size++;
+
+  return true;
+}
+
+void List::print() {
   Node *cur = this->firstNode;
   while (cur->next != nullptr) {
     cur = cur->next;
+    cout << cur->item << endl;
   }
-  cur->next = new Node{item, nullptr};
-  size++;
-  return true;
 }
--- practical-03/main.cpp
+++ practical-03/main.cpp
@@ -1,7 +1,17 @@
 #include "List.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
   List nameList;
-
+  nameList.add("Alice");
+  nameList.add("Jacky");
+  nameList.add("Wendy");
+  cout << endl;
+  nameList.print();
+  nameList.add(1, "Brenda");
+  cout << endl;
+  nameList.print();
   return 0;
 }
