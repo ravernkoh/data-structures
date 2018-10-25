f99bcf941c701cf6d90bb1a305e754feaeef5db6
Ravern Koh <ravernkoh@gmail.com>
2018-10-25 12:02:31 +0800

    Add specification of the List

--- practical-02/List.h
+++ practical-02/List.h
@@ -9,9 +9,25 @@ using namespace std;
 class List {
 public:
   List(int cap);
+
+  // Adds a person to the end of the list.
+  // PRE  len < cap
+  // POST Person added to end of the list.
   void add(Person *person);
+
+  // Removes a person from the specified index.
+  // PRE  index < len
+  // POST Person removed from list at the specified index.
   bool remove(int index);
+
+  // Returns the person from the specified index.
+  // PRE  index < len
+  // POST Returns the person at the specified index.
   Person *get(int index);
+
+  // Returns the length of the list.
+  // PRE  None.
+  // POST Returns the length of the list.
   int length();
 
 private:
