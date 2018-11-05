cdb1e59731c95570d36d45023d6fb088794291fb
Ravern Koh <ravernkoh@gmail.com>
2018-11-05 15:49:28 +0800

    Copy in Stack.h from doc

---
+++ practical-04/Stack.cpp
@@ -0,0 +1,3 @@
+#include "Stack.h"
+
+using namespace std;
---
+++ practical-04/main.cpp
@@ -0,0 +1,9 @@
+#include "Stack.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+  return 0;
+}
