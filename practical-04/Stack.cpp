2b98dd9cf06e6a7b31d55301160ed219586796b9
Ravern Koh <ravernkoh@gmail.com>
2018-11-05 15:55:14 +0800

    Implement basic operations

--- practical-04/Stack.cpp
+++ practical-04/Stack.cpp
@@ -1,3 +1,14 @@
 #include "Stack.h"
 
 using namespace std;
+
+Stack::Stack() { this->topNode = nullptr; }
+
+Stack::~Stack() { delete this->topNode; }
+
+bool Stack::isEmpty() { return this->topNode == nullptr; }
+
+bool Stack::push(ItemType &item) {
+  this->topNode = new Node{item, this->topNode};
+  return true;
+}
