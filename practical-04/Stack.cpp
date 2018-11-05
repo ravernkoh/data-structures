04452eec4edd12020fe103d5d4d2b6cad77900ae
Ravern Koh <ravernkoh@gmail.com>
2018-11-05 16:12:30 +0800

    Implement pop

--- practical-04/Stack.cpp
+++ practical-04/Stack.cpp
@@ -12,3 +12,24 @@ bool Stack::push(ItemType &item) {
   this->topNode = new Node{item, this->topNode};
   return true;
 }
+
+bool Stack::pop() {
+  ItemType *item;
+  bool result = this->pop(*item);
+  delete item;
+  return result;
+}
+
+bool Stack::pop(ItemType &item) {
+  if (this->topNode == nullptr) {
+    return false;
+  }
+
+  Node *oldNode = this->topNode;
+  item = oldNode->item;
+
+  this->topNode = this->topNode->next;
+
+  oldNode->next = nullptr;
+  delete oldNode;
+}
