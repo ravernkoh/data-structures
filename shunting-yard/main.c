657489a029e1488d81fd3101117603507974c249
Ravern Koh <ravernkoh@gmail.com>
2018-11-05 20:12:05 +0800

    Clean up make functions

--- shunting-yard/main.c
+++ shunting-yard/main.c
@@ -11,6 +11,7 @@
 #define MAX_INPUT_LENGTH 100
 
 typedef int token_kind_t;
+const token_kind_t TOKEN_INVALID = 0;
 const token_kind_t TOKEN_OP = 1;
 const token_kind_t TOKEN_NUM = 2;
 
@@ -20,21 +21,80 @@ const op_t OP_SUB = 2;
 const op_t OP_MUL = 3;
 const op_t OP_DIV = 4;
 
+// Display the op.
+const char *op_disp(op_t op) {
+  switch (op) {
+  case OP_ADD:
+    return "+";
+  case OP_SUB:
+    return "-";
+  case OP_MUL:
+    return "*";
+  case OP_DIV:
+    return "/";
+  }
+  return "INVALID";
+}
+
 typedef union {
-  int op;
+  op_t op;
   int num;
 } token_value_t;
+
+// Creates a new op value.
+token_value_t token_value_make_op(op_t op) {
+  token_value_t v;
+  v.op = op;
+  return v;
+}
+
+// Creates a new num value.
+token_value_t token_value_make_num(int num) {
+  token_value_t v;
+  v.num = num;
+  return v;
+}
 
 typedef union {
   token_kind_t kind;
   token_value_t value;
 } token_t;
 
+// Creates a new token.
+token_t token_make(token_kind_t kind, token_value_t value) {
+  token_t t;
+  t.kind = kind;
+  t.value = value;
+  return t;
+}
+
+// Creates a new invalid token.
+token_t token_make_invalid() {
+  token_t t;
+  t.kind = TOKEN_INVALID;
+  return t;
+}
+
+// Displays the token.
+const char *token_disp(token_t t) {
+  switch (t.kind) {
+  case TOKEN_OP:
+    return op_disp(t.value.op);
+  case TOKEN_NUM: {
+    char *num = (char *)malloc(sizeof(char) * MAX_LITERAL_LENGTH);
+    sprintf(num, "%i", t.value.num);
+    return num;
+  }
+  }
+  return "INVALID";
+}
+
 typedef struct {
   const char *source;
   char *literal;
 } scanner_t;
 
+// Creates a new scanner.
 scanner_t *scanner_make(const char *source) {
   scanner_t *t = (scanner_t *)malloc(sizeof(scanner_t));
   t->source = source;
@@ -49,11 +109,11 @@ char scanner_peek(scanner_t *t) { return *t->source; }
 void scanner_advance(scanner_t *t) { t->source = &t->source[1]; }
 
 // Returns the next token.
-token_t *scanner_next(scanner_t *t) {
+token_t scanner_next(scanner_t *t) {
   if (scanner_peek(t) == '\0') {
-    return NULL;
+    return token_make_invalid();
   }
-  return NULL;
+  return token_make(TOKEN_OP, token_value_make_op(OP_ADD));
 }
 
 int main() {
@@ -65,8 +125,9 @@ int main() {
     fgets(source, MAX_INPUT_LENGTH, stdin);
     source[strlen(source) - 1] = '\0';
 
+    // Create the scanner around the source.
     scanner_t *t = scanner_make(source);
-    printf("%d\n", scanner_next(t));
+    printf("%s\n", token_disp(scanner_next(t)));
   }
 
   return 0;
